45109c79fa0d99a4dff6f7e1150b5197e990be71
Knarik <knarik.yeritsyan@gmail.com>
2024-11-17 21:02:26 +0100

    Zip Filter app with C#

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.csvfoldername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pdffoldername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.brousescv = new System.Windows.Forms.Button();
+            this.brousepdf = new System.Windows.Forms.Button();
+            this.textcsv = new System.Windows.Forms.TextBox();
+            this.textpdf = new System.Windows.Forms.TextBox();
+            this.btnextractcsv = new System.Windows.Forms.Button();
+            this.btnextractpdf = new System.Windows.Forms.Button();
+            this.btnbrouse = new System.Windows.Forms.Button();
+            this.textFolder = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.csvfoldername);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.pdffoldername);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.brousescv);
+            this.panel1.Controls.Add(this.brousepdf);
+            this.panel1.Controls.Add(this.textcsv);
+            this.panel1.Controls.Add(this.textpdf);
+            this.panel1.Controls.Add(this.btnextractcsv);
+            this.panel1.Controls.Add(this.btnextractpdf);
+            this.panel1.Controls.Add(this.btnbrouse);
+            this.panel1.Controls.Add(this.textFolder);
+            this.panel1.Location = new System.Drawing.Point(12, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(715, 296);
+            this.panel1.TabIndex = 1;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Write a Folder name";
+            // 
+            // csvfoldername
+            // 
+            this.csvfoldername.Location = new System.Drawing.Point(372, 203);
+            this.csvfoldername.Name = "csvfoldername";
+            this.csvfoldername.Size = new System.Drawing.Size(168, 20);
+            this.csvfoldername.TabIndex = 16;
+            this.csvfoldername.TextChanged += new System.EventHandler(this.csvfoldername_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(401, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Write a Folder name";
+            // 
+            // pdffoldername
+            // 
+            this.pdffoldername.Location = new System.Drawing.Point(372, 141);
+            this.pdffoldername.Name = "pdffoldername";
+            this.pdffoldername.Size = new System.Drawing.Size(168, 20);
+            this.pdffoldername.TabIndex = 14;
+            this.pdffoldername.TextChanged += new System.EventHandler(this.pdffoldername_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Select a Path to be extracted";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Select a Path to be extracted";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Select a Folder to be extracted";
+            // 
+            // brousescv
+            // 
+            this.brousescv.Location = new System.Drawing.Point(546, 201);
+            this.brousescv.Name = "brousescv";
+            this.brousescv.Size = new System.Drawing.Size(75, 23);
+            this.brousescv.TabIndex = 10;
+            this.brousescv.Text = "...";
+            this.brousescv.UseVisualStyleBackColor = true;
+            this.brousescv.Click += new System.EventHandler(this.brousescv_Click);
+            // 
+            // brousepdf
+            // 
+            this.brousepdf.Location = new System.Drawing.Point(546, 141);
+            this.brousepdf.Name = "brousepdf";
+            this.brousepdf.Size = new System.Drawing.Size(75, 23);
+            this.brousepdf.TabIndex = 9;
+            this.brousepdf.Text = "...";
+            this.brousepdf.UseVisualStyleBackColor = true;
+            this.brousepdf.Click += new System.EventHandler(this.brousepdf_Click);
+            // 
+            // textcsv
+            // 
+            this.textcsv.Location = new System.Drawing.Point(40, 203);
+            this.textcsv.Name = "textcsv";
+            this.textcsv.Size = new System.Drawing.Size(308, 20);
+            this.textcsv.TabIndex = 8;
+            // 
+            // textpdf
+            // 
+            this.textpdf.Location = new System.Drawing.Point(40, 141);
+            this.textpdf.Name = "textpdf";
+            this.textpdf.Size = new System.Drawing.Size(308, 20);
+            this.textpdf.TabIndex = 7;
+            // 
+            // btnextractcsv
+            // 
+            this.btnextractcsv.Location = new System.Drawing.Point(627, 200);
+            this.btnextractcsv.Name = "btnextractcsv";
+            this.btnextractcsv.Size = new System.Drawing.Size(75, 23);
+            this.btnextractcsv.TabIndex = 6;
+            this.btnextractcsv.Text = "ExtractCSV";
+            this.btnextractcsv.UseVisualStyleBackColor = true;
+            this.btnextractcsv.Click += new System.EventHandler(this.btnextractcsv_Click);
+            // 
+            // btnextractpdf
+            // 
+            this.btnextractpdf.Location = new System.Drawing.Point(627, 141);
+            this.btnextractpdf.Name = "btnextractpdf";
+            this.btnextractpdf.Size = new System.Drawing.Size(75, 23);
+            this.btnextractpdf.TabIndex = 5;
+            this.btnextractpdf.Text = "ExtractPDF";
+            this.btnextractpdf.UseVisualStyleBackColor = true;
+            this.btnextractpdf.Click += new System.EventHandler(this.btnextractpdf_Click);
+            // 
+            // btnbrouse
+            // 
+            this.btnbrouse.Location = new System.Drawing.Point(548, 77);
+            this.btnbrouse.Name = "btnbrouse";
+            this.btnbrouse.Size = new System.Drawing.Size(75, 23);
+            this.btnbrouse.TabIndex = 4;
+            this.btnbrouse.Text = "...";
+            this.btnbrouse.UseVisualStyleBackColor = true;
+            this.btnbrouse.Click += new System.EventHandler(this.btnbrouse_Click);
+            // 
+            // textFolder
+            // 
+            this.textFolder.Location = new System.Drawing.Point(234, 80);
+            this.textFolder.Name = "textFolder";
+            this.textFolder.Size = new System.Drawing.Size(308, 20);
+            this.textFolder.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(789, 387);
+            this.Controls.Add(this.panel1);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnextractpdf;
+        private System.Windows.Forms.Button btnbrouse;
+        private System.Windows.Forms.TextBox textFolder;
+        private System.Windows.Forms.Button btnextractcsv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button brousescv;
+        private System.Windows.Forms.Button brousepdf;
+        private System.Windows.Forms.TextBox textcsv;
+        private System.Windows.Forms.TextBox textpdf;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox csvfoldername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox pdffoldername;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+using System.Text;
+using System.Linq;
+using System.Xml.Linq;
+using EPPlus;
+using  Excel = Microsoft.Office.Interop.Excel;
+using GemBox.Spreadsheet;
+using Spire.Xls;
+using Microsoft.Office.Core;
+
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+
+            DragDropEffects effects = DragDropEffects.None;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var path = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+                if (Directory.Exists(path))
+                    effects = DragDropEffects.Copy;
+                textFolder.Text = path;
+
+            }
+            e.Effect = effects;
+
+        }
+
+        private void btnbrouse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fbdun = new OpenFileDialog();
+            //fbdun.Description = "Select your path";
+            if (fbdun.ShowDialog() == DialogResult.OK)
+                textFolder.Text = fbdun.FileName;
+        }
+
+        private void btnextractpdf_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textFolder.Text))
+            {
+                MessageBox.Show("Please select your folder.");
+                textFolder.Focus();
+                return;
+            }
+            else if (string.IsNullOrEmpty(textpdf.Text))
+            {
+                MessageBox.Show("Please select your path.");
+                textFolder.Focus();
+                return;
+            }
+            else if (string.IsNullOrEmpty(pdffoldername.Text))
+            {
+                MessageBox.Show("Please write a Folder name.");
+                pdffoldername.Focus();
+                return;
+            }
+            string source = textFolder.Text;
+            string destination1 = textpdf.Text;
+            string destination2 = pdffoldername.Text;
+
+            string parent = Path.GetDirectoryName(source);
+            string name = Path.GetFileName(source);
+            //string foldername = Path.ChangeExtension(parent, null);
+            DirectoryInfo di = Directory.CreateDirectory(parent + "\\" + "extracted");
+            //string parentf = Path.GetDirectoryName(parent + "\\" + "extracted");
+            string sdestination = destination1 + "\\" + destination2;
+            ZipFile.ExtractToDirectory(source, parent + "\\" + "extracted");
+            Directory.CreateDirectory(sdestination);
+
+            foreach (var file in Directory.GetFiles(parent + "\\" + "extracted", "*.pdf"))
+            {
+                string fileName = Path.GetFileName(file);
+                string destFile = Path.Combine(sdestination, fileName);
+                File.Copy(file, destFile);
+                
+            }
+
+            di.Delete(true);
+            MessageBox.Show("PDF Files extracted successfully");
+
+
+        }
+
+        private void btnextractcsv_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textFolder.Text))
+            {
+                MessageBox.Show("Please select your folder.");
+                textFolder.Focus();
+                return;
+            }
+            else if (string.IsNullOrEmpty(textcsv.Text))
+            {
+                MessageBox.Show("Please select your path.");
+                textFolder.Focus();
+                return;
+            }
+            else if (string.IsNullOrEmpty(csvfoldername.Text))
+            {
+                MessageBox.Show("Please write a Folder name.");
+                csvfoldername.Focus();
+                return;
+            }
+
+            string source = textFolder.Text;
+            string destination1 = textcsv.Text;
+            string destination2 = csvfoldername.Text;
+
+            string parent = Path.GetDirectoryName(source);
+            string name = Path.GetFileName(source);
+            //string foldername = Path.ChangeExtension(parent, null);
+            DirectoryInfo di = Directory.CreateDirectory(parent + "\\" + "extracted");
+            DirectoryInfo si = Directory.CreateDirectory(parent + "\\" + "csv");
+            string sdestination = destination1 + "\\" + destination2;
+            ZipFile.ExtractToDirectory(source, parent + "\\" + "extracted");
+           
+
+            Directory.CreateDirectory(sdestination);
+            foreach (var file in Directory.GetFiles(parent + "\\" + "extracted", "*.csv"))
+            {
+                string fileName = Path.GetFileName(file);
+                string destFile = Path.Combine(source, parent + "\\" + "csv", fileName);
+                File.Copy(file, destFile);
+
+            }
+            di.Delete(true);
+
+            var date = DateTime.Now.Date.ToString("dd MM yyyy");
+            string sourceFile = parent + "\\" + "csv";
+            //string date = DateTime.Today.ToString();
+            string destinationFile = sdestination + "\\" + date + ".csv";
+
+            string[] filePaths = Directory.GetFiles(sourceFile);
+            using (StreamWriter fileDest = new StreamWriter(destinationFile, true))
+            {
+                int i;
+                int filenumber = Directory.GetFiles(sourceFile).Length;
+                for (i = 0; i < filenumber; i++)
+                {
+                    string file = filePaths[i];
+
+                    string[] lines = File.ReadAllLines(file);
+
+                    if (i > 0)
+                    {
+                        lines = lines.ToArray();
+                    }
+
+                    foreach (string line in lines)
+                    {
+                        fileDest.WriteLine(line);
+                    }
+                }
+
+                fileDest.Close();
+            }
+
+            si.Delete(true);
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+            ExcelFile.Load(destinationFile).Save(sdestination + "\\" + date + ".xlsx");
+            File.Delete(destinationFile);
+
+            //MessageBox.Show(date);
+            MessageBox.Show("CSV Files extracted successfully");
+
+
+            /*string pathToHeader = @"C:\Users\Knarik\Desktop\trew\Sample-0001.csv";
+            //string pathToNoHeader = @"C:\Users\Knarik\Desktop\trew\Sample-0002.csv";
+
+
+            //string headerText = File.ReadAllText(pathToHeader);
+            //string noHeaderText = File.ReadAllText(pathToNoHeader);
+
+            //File.WriteAllText(@"C:\Users\Knarik\Desktop\iotswr\combined.csv", string.Concat(headerText, noHeaderText));*/
+            /*var app = new Excel.Application();
+            app.Visible = false;
+            app.DisplayAlerts = false;
+            var workbook = app.Workbooks.Add(Type.Missing);
+            workbook.SaveAs(sdestination + "\\" + date + ".xlsx");
+            workbook.Close();
+            app.Quit();
+            string destinationFilex = sdestination + "\\" + date + ".xlsx";*/
+
+            /*string[] filePaths = Directory.GetFiles(sourceFile);
+            using (StreamWriter fileDest = new StreamWriter(destinationFilex, true))
+            {
+                //fs
+                int i;
+                int filenumber = Directory.GetFiles(sourceFile).Length;
+                for (i = 0; i < filenumber; i++)
+                {
+                    string file = filePaths[i];
+
+                    string[] lines = File.ReadAllLines(file);
+
+                    if (i > 0)
+                    {
+                        lines = lines.ToArray(); // Skip header row for all but first file
+                    }
+
+                    foreach (string line in lines)
+                    {
+                        fileDest.WriteLine(line);
+                    }
+                }
+
+                fileDest.Close();
+            }*/
+
+
+
+            /*string[] filePaths =  Directory.GetFiles(@"C: \Users\Knarik\Desktop\FilePorc");
+             StreamWriter fileDest = new StreamWriter(@"C:\Users\Knarik\Desktop\iotswr\combined.csv", true);
+
+             int i;
+            int filenumber = Directory.GetFiles(@"C: \Users\Knarik\Desktop\FilePorc").Length;
+             for (i = 0; i < filenumber; i++)
+             {
+                 string file = filePaths[i];
+
+                 string[] lines = File.ReadAllLines(file);
+
+                if (i > 0)
+                 {
+                     lines = lines.Skip(1).ToArray(); // Skip header row for all but first file
+                 }
+
+            foreach (string line in lines)
+            {
+                fileDest.WriteLine(line);
+            }
+        }
+
+        fileDest.Close();*/
+
+            //string date = DateTime.Today.ToString();
+
+        }
+       
+
+        private void brousepdf_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbdun = new FolderBrowserDialog();
+            //fbdun.Description = "Select your path";
+            if (fbdun.ShowDialog() == DialogResult.OK)
+                textpdf.Text = fbdun.SelectedPath;
+        }
+
+        private void brousescv_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbdun = new FolderBrowserDialog();
+            //fbdun.Description = "Select your path";
+            if (fbdun.ShowDialog() == DialogResult.OK)
+                textcsv.Text = fbdun.SelectedPath;
+        }
+
+        private void pdffoldername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void csvfoldername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    internal class srting
+    {
+    }
+}
